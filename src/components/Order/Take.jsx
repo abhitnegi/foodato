775c78b1dcc8950620eea6cf421a4809f2b64a11
Abhit <abhitnegi@gmail.com>
2023-06-30 12:58:55 -0700

    Replacing css of Home.jsx

--- src/components/Order/Take.jsx
+++ src/components/Order/Take.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Take = (props) => {
     const { data } = props;
   return (
-    <div>
+    <div className='search-div'>
       <div className="product">
         <img src={data.productImage} alt="" className='menu-image'/>
         <div className="description">
